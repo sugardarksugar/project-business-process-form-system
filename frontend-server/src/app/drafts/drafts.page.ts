035d083305f5beba8afa7de4676ff6b12ed32295
Lai Fong Lau <laifonglau2004@yahoo.com.hk>
2023-01-13 17:35:57 +0800

    update

--- frontend-server/src/app/drafts/drafts.page.ts
+++ frontend-server/src/app/drafts/drafts.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemReorderEventDetail } from '@ionic/angular';
 
-interface Teammate {
-  username: string
+interface Form {
+  title: string
+  fields: Field[]
 }
 
-interface Checker {
-  username: string
-}
-
-interface Task {
-  content: string
+interface Field {
+  title: string
+  type: string
+  desc: string
 }
 
 @Component({
@@ -18,22 +18,33 @@ interface Task {
   styleUrls: ['./drafts.page.scss'],
 })
 export class DraftsPage implements OnInit {
-  // taskList: Task[] = [{ title: '' }]
 
-  tasks: Task[] = []
+  form: Form = {
+    title: '',
+    fields: []
+  }
 
-  constructor() { }
+  constructor() {
+  }
+
+  reorderForm(ev: CustomEvent<ItemReorderEventDetail>) {
+    console.log(ev);
+
+  }
+  handleReorder(ev: CustomEvent<ItemReorderEventDetail>) {
+    ev.detail.complete()
+  };
+
 
   ngOnInit() {
-    this.addTask()
+    this.addField()
   }
 
-  addTask() {
-    this.tasks.push({ content: '' })
+  addField() {
+    this.form.fields.push({ title: '', type: '', desc: '' })
   }
 
-  get json() {
+  get json(): string {
     return JSON.stringify(this, null, 2)
   }
-
 }
